c2d60e21f67527fdcbdad56b6ecc66d8f3982751
DYM <grunge.rock.seven@gmail.com>
2017-01-04 13:06:03 +0900

    update

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :microposts
   validates :name, presence: true #Replace FILL_IN with the tight code.
-  validates :em, presence: true
+  validates :email, presence: true
 
 end
